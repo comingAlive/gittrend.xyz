34773e4f5556f1c6f6d25512e174429497cbc0ad
Max <max@aiwon.org>
2023-01-27 17:09:36 +0300

    added aiw

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,8 @@ const Footer = () => {
     <footer className="mt-4 h-auto font-medium bg-white rounded border-l-8 border-gray-500 dark:bg-black">
       <div className="flex justify-between items-center">
         <div className="w-full text-center py-2">
-          <span className="mr-1">Created by</span>
           <Link legacyBehavior href={"https://t.me/aiwon"}>
-            <a className="text-blue-600 dark:text-blue-400">Max</a>
+            <a className="text-blue-600 dark:text-blue-400">@aiwon</a>
           </Link>
           .
         </div>
