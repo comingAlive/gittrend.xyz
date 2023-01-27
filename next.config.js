77814287f519701111a7f0f1b9c397ed09d4e880
Max <max@aiwon.org>
2023-01-27 16:02:19 +0300

    git explore to git trend

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ module.exports = withPlugins([], {
   i18n: { locales: ["en-US"], defaultLocale: "en-US" },
   images: { domains: ["github.com"] },
 
-  webpack(config, { dev, isServer }) {
-    // Replace React with Preact in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
+  // webpack(config, { dev, isServer }) {
+  //   // Replace React with Preact in client production build
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: "preact/compat",
+  //       "react-dom/test-utils": "preact/test-utils",
+  //       "react-dom": "preact/compat",
+  //     });
+  //   }
+  //
+  //   return config;
+  // },
 });
