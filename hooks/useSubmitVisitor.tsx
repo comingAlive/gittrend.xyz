983343608325409b5515ec6740ff4e7eac2e1b97
Max <max@aiwon.org>
2023-01-28 00:56:16 +0300

    added aiw

--- hooks/useSubmitVisitor.tsx
+++ hooks/useSubmitVisitor.tsx
@@ -5,11 +5,11 @@ const useSubmitVisitor = () => {
   useEffect(() => {
     const main = async () => {
       const r = await fetch("https://ipapi.co/json");
-      const json = await r.json();
+      const json = await r?.json();
       await supabase.from("visitors").insert({
-        ip: json.ip,
-        region: json.region,
-        url: window.location.href,
+        ip: json?.ip,
+        region: json?.region,
+        url: window?.location.href,
       });
     };
     main();
